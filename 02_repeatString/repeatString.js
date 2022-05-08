a486f9e8bcec9f130d5e0612a12170db78fffb5e
Akshat <smartakshat007@gmail.com>
2022-05-08 23:53:31 +0530

    edit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let i=0;
+    let temp="";
+    let text=string;
+    if(num<0)
+        return "ERROR";
+    while(i<num) {
+        temp+=text;
+        i++;
+    }
+    return temp;
 };
 
 // Do not edit below this line
